a937a81a88dac9e06f72bb8079137ec79c14a8bc
sanaspx <sanaspx@yandex.ru>
2021-01-22 00:17:11 +0500

    forms two

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all ())
+    title = forms.CharField(max_length=150, label='Название')
+    content = forms.CharField(label='Текст', required=False)
+    is_published = forms.BooleanField(label='Опубликованно', initial=True)
+    category = forms.ModelChoiceField(empty_label='Выберите категорию', label='Категория', queryset=Category.objects.all () )
